d8a61c1fbabd99ba69b1d265957c258d9dd6e82b
Noelia Doman <chavezn@mail.gvsu.edu>
2026-04-20 22:01:52 -0400

    Refactor Creamer component to use beverage color

--- src/components/Creamer.vue
+++ src/components/Creamer.vue
@@ -1,16 +1,17 @@
 <template>
-  <div v-if="showCream" class="froth" :style="{ '--creamer-color': creamerColor }">
-    <div v-for="i in 5" :key="i" class="foam"></div>
+  <div class="froth">
+    <div
+      v-for=" in 5"
+      class="foam"
+      :style="{ backgroundColor: beverageStore.currentCreamer?.color }"
+    ></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
 import { useBeverageStore } from "../stores/beverageStore";
 
 const beverageStore = useBeverageStore();
-const showCream = computed(() => beverageStore.currentCreamer?.name !== "No Cream");
-const creamerColor = computed(() => beverageStore.currentCreamer?.color ?? "transparent");
 </script>
 <style lang="scss" scoped>
 .froth {
@@ -19,11 +20,12 @@ const creamerColor = computed(() => beverageStore.currentCreamer?.color ?? "tran
   position: relative;
   height: 20%;
   width: 100%;
+  background-color: #c6c6c6;
   animation: pour-tea 2s 2s forwards;
 }
 .foam {
   display: block;
-  background: var(--creamer-color);
+  background: #e4e0d2;
   border-radius: 30px;
   height: 40px;
   width: 40px;
