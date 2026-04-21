7cdb64dbffdede2c896ce166e03a0838947b4116
Noelia Doman <chavezn@mail.gvsu.edu>
2026-04-20 22:40:11 -0400

    Refactor app initialization for simplicity
    Simplified app initialization by removing the startApp function and directly mounting the app.

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,7 @@ import "./styles/mug.scss";
 import { createPinia } from "pinia";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import App from "./App.vue";
-import { useBeverageStore } from "./stores/beverageStore";
-
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState);
 
-async function startApp() {
-  const app = createApp(App);
-  app.use(pinia);
-
-  const beverageStore = useBeverageStore(pinia);
-  await beverageStore.init();
-
-  app.mount("#app");
-}
-
-startApp();
+createApp(App).use(pinia).mount("#app");
