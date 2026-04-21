1e448dbdba239671fbfcae84f4a14d8ebfafad4d
Noelia Doman <chavezn@mail.gvsu.edu>
2026-04-20 22:04:57 -0400

    Change Firebase configuration settings
    Updated Firebase configuration with new project details.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,14 @@
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFQJEAXROqAmjXamOfJjR7uIQRX2CUBDk",
-  authDomain: "beveragemaker.firebaseapp.com",
-  projectId: "beveragemaker",
-  storageBucket: "beveragemaker.firebasestorage.app",
-  messagingSenderId: "822763540532",
-  appId: "1:822763540532:web:5b9bc7f755dfb12c6aba61",
-  measurementId: "G-FHE461P26X"
+  apiKey: "AIzaSyCpzQqezaUrXlrnvaN-4Q_SO66V515L-4g",
+  authDomain: "beverageshop-ace5c.firebaseapp.com",
+  projectId: "beverageshop-ace5c",
+  storageBucket: "beverageshop-ace5c.firebasestorage.app",
+  messagingSenderId: "826745717432",
+  appId: "1:826745717432:web:7f93cdea071298b06d2a4d",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,4 +16,3 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
-export default db;
