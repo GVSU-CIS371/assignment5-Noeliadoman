93f8af9694779288e9e9b78d7e3ff37ba3ca9ece
Noelia Doman <chavezn@mail.gvsu.edu>
2026-04-20 21:12:00 -0400

    Refactor Beverage.vue to always display Creamer and Syrup
    Refactored Beverage.vue to always show Creamer and Syrup components. Removed conditional rendering based on store values.

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -4,10 +4,10 @@
     <Hot v-else />
     <Contents>
       <template v-slot:top>
-        <Creamer v-if="showCreamer" />
+        <Creamer />
       </template>
       <template v-slot:mid>
-        <Syrup v-if="showSyrup" />
+        <Syrup />
       </template>
       <template v-slot:bottom>
         <Base />
@@ -15,9 +15,7 @@
     </Contents>
   </Mug>
 </template>
-
 <script setup lang="ts">
-import { computed } from "vue";
 import Contents from "./Contents.vue";
 import Mug from "./Mug.vue";
 import Syrup from "./Syrup.vue";
@@ -25,12 +23,9 @@ import Base from "./Base.vue";
 import Creamer from "./Creamer.vue";
 import Hot from "./Hot.vue";
 import Cold from "./Cold.vue";
-import { useBeverageStore } from "../stores/beverageStore";
 
-type Props = { isIced: boolean };
+type Props = {
+  isIced: boolean;
+};
 defineProps<Props>();
-
-const store = useBeverageStore();
-const showCreamer = computed(() => store.currentCreamer.name !== "No Cream");
-const showSyrup   = computed(() => store.currentSyrup.name   !== "No Syrup");
 </script>
