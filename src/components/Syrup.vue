0b24477398436905447260364d30b6edbc4fc86c
Noelia Doman <chavezn@mail.gvsu.edu>
2026-04-20 21:12:59 -0400

    Update Syrup component to conditionally show syrup

--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,26 +1,15 @@
 <template>
-  <div class="syrup" :style="syrupStyle"></div>
+  <div v-if="showSyrup" class="syrup" :style="{ backgroundColor: syrupColor }"></div>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
 import { useBeverageStore } from "../stores/beverageStore";
+
 const beverageStore = useBeverageStore();
-
-const syrupStyle = computed(() => {
-  const color = beverageStore.currentSyrup.color;
-  return {
-    background: `repeating-linear-gradient(
-      45deg,
-      ${color},
-      ${color} 10px,
-      rgba(225, 207, 149, 1) 10px,
-      rgba(225, 207, 149, 1) 20px
-    )`,
-  };
-});
+const showSyrup = computed(() => beverageStore.currentSyrup?.name !== "No Syrup");
+const syrupColor = computed(() => beverageStore.currentSyrup?.color ?? "#c6c6c6");
 </script>
-
 <style lang="scss" scoped>
 .syrup {
   transform: translateY(400%);
@@ -29,6 +18,5 @@ const syrupStyle = computed(() => {
   height: 20%;
   animation: pour-tea 2s 1s forwards;
   z-index: 2;
-  transition: background 0.5s ease;
 }
 </style>
